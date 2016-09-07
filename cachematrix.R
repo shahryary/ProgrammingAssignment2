d99be16fc4e4db97b3e2b867ca6b2d0b60599d88
shahryary <shahryary@gmail.com>
2016-09-08 00:44:34 +0300

    Added some comments.

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,15 @@
-## Peer Graded Assignment: Programming Assignment 2: Lexical Scoping
+# Peer Graded Assignment: Programming Assignment 2: Lexical Scoping
 
-## Put comments here that give an overall description of what your
-## functions do
+## This code is my solution to second assignments from the R programming on Coursera.
 
-## Write a short comment describing this function
+## It's better to cache the results when we running time consuming computations 
+## instead of calculate it agian.
+
+## According to the course in Coursera and content in week 3 we see the computing matrix is costly, 
+## so we can compute it and then cache the inverse of matrix. 
+
+## So, our first function as in templete is called "makeCacheMatrix" that we'll  
+## creates a  “matrix” object and thencache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
 
