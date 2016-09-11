c39b8dc149c07a8576ea4af150b083f887611487
shahryary <shahryary@gmail.com>
2016-09-11 15:58:25 +0300

    Added "makeCacheMatrix" function.

--- cachematrix.R
+++ cachematrix.R
@@ -9,15 +9,32 @@
 ## so we can compute it and then cache the inverse of matrix. 
 
 ## So, our first function as in templete is called "makeCacheMatrix" that we'll  
-## creates a  “matrix” object and thencache its inverse.
+## creates a  “matrix” object and then cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  ## x  is a square invertible matrix
+  ## and will be return: a list containing functions to
+  ##              1. set the matrix
+  ##              2. get the matrix
+  ##              3. set the inverse
+  ##              4. get the inverse
+  ##         this list will be pass in input to cacheSolve()
+  
+  environment = NULL
+  set = function(y) {
+    # by using '<<-' we can assign a value to an object in environment 
+    x <<- y
+    environment <<- NULL
+  }
+  # Note: set() is a function, and part of what it does is to set environment to NULL.
+  # But environment is defined outside of set(), that's why we need '<<-'  aslo in set function.
+  get = function() x
+  setinverse = function(inverse) environment <<- inverse 
+  getinverse = function() environment
+  list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
+  
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
